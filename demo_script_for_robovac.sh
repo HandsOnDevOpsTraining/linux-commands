64a24cc1b551e76bf39a4017df122e2de524f34d
lankamadangit <handsondevopstraining@outlook.com>
2025-02-15 22:40:02 +1100

    Demo script

---
+++ demo_script_for_robovac.sh
@@ -0,0 +1,6 @@
+enter-room blue #Going inside red room
+clean-room blue
+exit-room blue #Coming out of red room
+enter-room green #Going inside red room
+clean-room green
+exit-room green #Coming out of red room
---
+++ first_script.sh
@@ -0,0 +1,9 @@
+cd blue #Going inside red room
+echo "I am inside blue folder"
+cd .. #Coming out of red room
+cd green #Going inside green room
+echo "I am inside  green folder"
+cd .. #Coming out of green room
+cd blue #Going inside blue room
+echo "I am inside blue folder"
+cd .. #Coming out of blue room
